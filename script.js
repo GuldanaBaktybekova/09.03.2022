2711c2a1e763c3901a6d837c2c0bb99793e8a889
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-09 14:31:04 +0600

    Added span

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const main = document.querySelector('main');
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function displayList() {
+  main.innerHTML = "";
   list.forEach(item => {
     const div = document.createElement('div');
     div.textContent = item;
@@ -10,3 +11,15 @@ function displayList() {
   });
 }
 displayList();
+displayList();
+
+
+
+function sortDescending() {
+  main.textContent--;
+  list.forEach(sort => {
+    const span = document.createElement('span');
+    span.textContent = sort;
+    main.append(span);
+  })
+}
