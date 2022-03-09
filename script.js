9adfa6ca0422792ecad045529449900453af2591
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-09 14:44:32 +0600

    Added addeventlistener

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function displayList() {
   main.innerHTML = "";
-  list.forEach(item => {
-    const div = document.createElement('div');
+  list.forEach((item) => {
+    const div = document.createElement("div");
     div.textContent = item;
     main.append(div);
-    
   });
 }
 displayList();
 displayList();
 
+// function sortDescending() {
+//   list.sort((a, b) => {
+//     if (a > b) {
+//       return -1;
+//     }
+//     else {
+//       return 1;
 
+//     }
+//   });
 
-function sortDescending() {
-  list.sort((a, b) => {
-    if (a > b) {
-      return -1;
-    } 
-    else {
-      return 1;
-   
-    }
-  }
+// }
+// sortDescending();
+// displayList();
 
-}
-sortDescending();
-displayList();
+document
+  .querySelector("#sort-descending")
+  .addEventListener("click", function () {
+    sortDescending();
+    displayList();
+  });
