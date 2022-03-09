396a78fe57e7e7b6524e22e8ebdd8471ca1ac923
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-09 15:18:14 +0600

    Added show-odd

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-const main = document.querySelector("main");
+const main = document.querySelector('main');
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function displayList() {
+function displayList(list) {
   main.innerHTML = "";
-  list.forEach((item) => {
-    const div = document.createElement("div");
+  list.forEach(item => {
+    const div = document.createElement('div');
     div.textContent = item;
     main.append(div);
+    
   });
 }
-displayList();
-displayList();
 
-// function sortDescending() {
-//   list.sort((a, b) => {
-//     if (a > b) {
-//       return -1;
-//     }
-//     else {
-//       return 1;
 
-//     }
-//   });
 
-// }
-// sortDescending();
-// displayList();
 
+function sortDescending() {
+  list.sort((a, b) => {
+    if (a > b) {
+      return -1;
+    } 
+    else {
+      return 1;
+   
+    }
+  });
+
+}
+
+function sortAscening() {
+  list.sort((a, b) => a < b ? -1 : 1);
+}
+function showOdd() {
+  return  list.filter(item => item % 2 != 0)
+   
+ };
+
+
+displayList(list);
 document
   .querySelector("#sort-descending")
   .addEventListener("click", function () {
     sortDescending();
-    displayList();
+    displayList(list);
   });
+   
+  document
+  .querySelector("#sort-ascending")
+  .addEventListener("click", function () {
+    sortAscening();
+    displayList(list);
+   
+  });
+  document
+  .querySelector("#show-odd")
+  .addEventListener("click", function () {
+    
+    displayList(showOdd());
+   
+  });
+
+ 
+     
